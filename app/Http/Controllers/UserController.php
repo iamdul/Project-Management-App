29c6e576fa28e6da43fc12578a00e93105046261
iamdul <kalharidulanga@gmail.com>
2024-07-14 14:04:05 +0530

    implement user CRUD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserCRUDResource;
+use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -13,7 +18,25 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $query=User::query();
+
+        $sortField = request("sort_field",'created_at');
+        $sortDirection = request("sort_direction",'desc');
+
+        if(request('name')){
+            $query ->where("name","like","%".request("name")."%");
+        }
+
+        if(request('email')){
+            $query ->where("email","like","%".request("email")."%");
+        }
+
+        $users = $query -> orderBy($sortField,$sortDirection)->paginate(10) -> onEachSide(1);
+        return inertia("User/Index",[
+            'users' => UserCRUDResource::collection($users),
+            'queryParams' => request()->query() ? : null,
+            'success' =>session('success'),
+        ]);
     }
 
     /**
@@ -21,7 +44,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return inertia("User/Create");
     }
 
     /**
@@ -29,7 +52,12 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request -> validated();
+        $data['email_verified_at'] = time();
+        $data['password'] = bcrypt($data['password']);
+        User::create($data);
+
+        return to_route('user.index')->with('success','User was created');
     }
 
     /**
@@ -45,7 +73,9 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return inertia('User/Edit',[
+            'user' => new UserCRUDResource($user),
+        ]);
     }
 
     /**
@@ -53,7 +83,16 @@ class UserController extends Controller
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        $data = $request -> validated();
+        $password = $data['password'] ?? null;
+        if($password){
+            $data['password'] = bcrypt($password);
+        }else{
+            unset($data['password']);
+        }
+
+        $user -> update($data);
+        return to_route('user.index')->with('success', "User \"$user->name\" was updated");
     }
 
     /**
@@ -61,6 +100,9 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $name = $user ->name;
+        $user ->delete();
+
+        return to_route('user.index')->with('success', "User \"$name\" was deleted");
     }
 }
